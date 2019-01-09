b5b30f50585ecedc5d388f95e9774ad5d7189d18
Pavel I <ipa0486@gmail.com>
2019-01-09 10:38:23 +0200

    add normilize styles yarn add node-sass --save

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux'
 import './index.css';
+import './normalize.scss';
 import App from './containers/App';
 import { store } from './store/configureStore'
 import * as serviceWorker from './serviceWorker';
